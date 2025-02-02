6f34578633a695aca5bdb58b4ce81de87474bda2
Dimitar Vachev <d.vachev2002@gmail.com>
2025-02-02 16:03:00 +0200

    yep

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ AOS.init();
 
 
 var typedHeader = new Typed(".multiText", {
-    strings: ['😮Student😮', '🤓Programmer🤓 ','🫠Dreamer🫠','👨🏽‍💼Entrepreneur👨🏽‍💼'],
+    strings: ['😮Student😮', '🤓Programmer🤓 ', '🫠Dreamer🫠', '👨🏽‍💼Entrepreneur👨🏽‍💼'],
     typeSpeed: 200,
     backSpeed: 50,
     backDelay: 1000,
     loop: true
-  });
+});
 
-  var typedHello = new Typed(".multiTText", {
-    strings: ['👋Hello👋 ', '👋Bonjour👋','👋Zdravstvuyte👋','👋Nǐn hǎo👋','👋Salve👋','👋Asalaam alaikum👋'],
+var typedHello = new Typed(".multiTText", {
+    strings: ['👋Hello👋 ', '👋Bonjour👋', '👋Zdravstvuyte👋', '👋Nǐn hǎo👋', '👋Salve👋', '👋Asalaam alaikum👋'],
     typeSpeed: 200,
     backSpeed: 50,
     backDelay: 1000,
     loop: true
-  });
+});
